e1f3789392d69749170efc6ccc99c2c3d4a71c40
tejasdotchavan <tejas@digitalbluefoam.com>
2022-07-06 09:34:45 +0800

    add wednesday folder, revert to sqaure grid

---
+++ wednesday/public/libs/utils.js
@@ -0,0 +1,76 @@
+
+const {
+    Shape,
+    Vector3,
+    ExtrudeGeometry,
+    Mesh,
+    EdgesGeometry,
+    BoxGeometry,
+    LineSegments,
+    LineCurve,
+    MeshStandardMaterial,
+  } = THREE;
+
+function extrude({ polygon, holes, depth, translation, scale, rotation, material, uuid }) {
+    if (!polygon.length) {
+      return null;
+    }
+  
+    const shape = getShape(polygon);
+  
+    if (holes) {
+      if (holes.length) {
+        for (const hole of holes) {
+          if (hole.length) {
+            shape.holes.push(getShape(hole.reverse()));
+          }
+        }
+      }
+    }
+  
+    const geometry = new ExtrudeGeometry(shape, { depth, bevelEnabled: false });
+    geometry.rotateX(-Math.PI / 2);
+  
+    /*if (rotation) {
+          geometry.rotateX(rotation.x)
+          geometry.rotateY(rotation.y)
+          geometry.rotateZ(rotation.z)
+      }*/
+  
+    // if(translation) geometry.translate(translation.x, translation.y, translation.z)
+    // if(scale) geometry.scale(scale.x, scale.y, scale.z)
+  
+    const mesh = new Mesh(geometry, material ? material.clone() : new MeshStandardMaterial());
+  
+    if (rotation) {
+      mesh.rotateX(rotation.x);
+      mesh.rotateY(rotation.y);
+      mesh.rotateZ(rotation.z);
+    }
+  
+    if (scale) mesh.scale(scale.x, scale.y, scale.z);
+    if (translation) mesh.position.set(translation.x, translation.y, translation.z);
+  
+    // if (scale) mesh.scale.set(scale.x, scale.y, scale.z)
+  
+    if (uuid) mesh.uuid = uuid;
+  
+    return mesh;
+  }
+
+  function getShape(cell) {
+    const shape = new Shape();
+    const o = cell[0];
+  
+    if (o) {
+      shape.moveTo(o.x, -o.z);
+  
+      for (var i = 1; i < cell.length; i++) {
+        var pt = cell[i];
+        shape.lineTo(pt.x, -pt.z);
+      }
+  
+      shape.lineTo(o.x, -o.z);
+      return shape;
+    }
+  }
---
+++ wednesday/src/app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+// var socket = require("socket.io");
+const io = require("socket.io")(http);
+
+//central state of voxel-painter
+const state = {}
+
+app.use(express.static('../public/'));
+
+app.get('/',function(req, res) {
+    res.sendFile('index.html', {root: '../public'});
+});
+
+io.sockets.on('connection', newConnection);
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+
+function newConnection(socket){
+
+	socket.userData = { avatar:null, position: null, color: null};
+
+	console.log('New player connected.', socket.id)
+ 
+	socket.emit('player:joined', { id:socket.id });
+	// socket.broadcast.emit('player:new-remote', { id: socket.id });
+	
+    socket.on('disconnect', function(){
+		socket.broadcast.emit('player:left', { id: socket.id });
+    });	
+
+	socket.on("init", initPlayer)
+	socket.on("update", updatePlayer);
+	socket.on("add", placeBlock);
+	socket.on("remove", removeBlock);
+
+	function initPlayer(data) {
+		console.log(`socket.init ${data.avatar}`);
+		socket.userData.avatar = data.avatar;
+		socket.userData.color = data.color;
+		socket.userData.position = data.position;
+	}
+
+	function updatePlayer(data) {
+		// console.log('updatePlayer:: ', data.position)
+		socket.userData.position = data.position;
+		// socket.broadcast.emit("update", data);
+	}
+
+	function placeBlock(data) {
+		// console.log('add:: ', data)
+		socket.broadcast.emit("add", data);
+	}
+	
+	function removeBlock(data) {
+		// console.log('remove:: ', data)
+		socket.broadcast.emit("remove", data);
+	}
+
+}
+
+
+
+setInterval(function(){
+	const clients = Array.from(io.sockets.sockets).map(socket => socket[1]);
+    let pack = [];
+	
+	
+    for(const socket of clients){
+		// console.log('Remote socket:: ', socket.userData)
+		//this didn't work for some reason
+		// const socket = nsp.connected[id];
+
+		if (socket.userData.avatar !== null){
+			pack.push({
+				id: socket.id,
+				avatar: socket.userData.avatar,
+				color: socket.userData.color,
+				position: socket.userData.position,
+			});    
+		}
+    }
+	// console.log('Sending remote data:: ', pack.map(p => p.position))
+	if (pack.length>0) io.emit('remoteData', pack);
+}, 40);
+
+
+
