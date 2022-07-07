fb7eb549ab0e11b5c1f4dce24b2a9b78502ad673
Sayjel Vijay Patel <sayjel.v.patel@gmail.com>
2022-07-07 15:33:54 +0800

    updates

--- thursday/public/game.js
+++ thursday/public/game.js
@@ -182,7 +182,6 @@ class Game {
     onWindowResize() {
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
-
         this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
