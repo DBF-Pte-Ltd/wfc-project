560cfe3b238e681b843ea98f7f5adc19818f93f0
tejasdotchavan <tejas@digitalbluefoam.com>
2022-07-07 12:10:08 +0800

    remove double functions

--- thursday/public/game.js
+++ thursday/public/game.js
@@ -76,7 +76,7 @@ class Game {
                 voxel.uuid = value.uuid;
                 game.scene.add(voxel);
                 game.objects.push(voxel);
-                // startOver();
+                startOver();
                 break;
             case "remove":
                 const objectIndex = game.objects.findIndex(
@@ -226,42 +226,46 @@ class Game {
         });
         this.updateRemotePlayers(dt);
 
-        if (handposeModel && videoDataLoaded) {
-            // model and video both loaded
-            handposeModel.estimateHands(capture).then(function(_hands) {
-                // we're handling an async promise
-                // best to avoid drawing something here! it might produce weird results due to racing
-                myHands = _hands; // update the global myHands object with the detected hands
-                if (!myHands.length) {
-                    // haven't found any hands
-                    statusText = "Show some hands!";
-                } else {
-                    // display the confidence, to 3 decimal places
-                    statusText =
-                        "Confidence: " +
-                        Math.round(myHands[0].handInViewConfidence * 1000) / 1000;
-                }
+        if(false) {
 
-                // tell the server about our updates!
-                game.player.hands = myHands;
-                if (game.player.hands[0])
-                    game.player.hands[0].landmarks.forEach((l) => (l[2] += 200));
-                game.player.updateSocket();
-            });
+          if (handposeModel && videoDataLoaded) {
+              // model and video both loaded
+              handposeModel.estimateHands(capture).then(function(_hands) {
+                  // we're handling an async promise
+                  // best to avoid drawing something here! it might produce weird results due to racing
+                  myHands = _hands; // update the global myHands object with the detected hands
+                  if (!myHands.length) {
+                      // haven't found any hands
+                      statusText = "Show some hands!";
+                  } else {
+                      // display the confidence, to 3 decimal places
+                      statusText =
+                          "Confidence: " +
+                          Math.round(myHands[0].handInViewConfidence * 1000) / 1000;
+                  }
+  
+                  // tell the server about our updates!
+                  game.player.hands = myHands;
+                  if (game.player.hands[0])
+                      game.player.hands[0].landmarks.forEach((l) => (l[2] += 200));
+                  game.player.updateSocket();
+              });
+          }
+  
+          //draw on 2D canvas for hands
+          dbg.clearRect(0, 0, dbg.canvas.width, dbg.canvas.height);
+          dbg.save();
+          dbg.fillStyle = "red";
+          dbg.strokeStyle = "red";
+          //   dbg.drawImage(capture,0,0);
+          drawHands(myHands);
+          dbg.restore();
+          dbg.save();
+          dbg.fillStyle = "red";
+          //   dbg.fillText(statusText,2,60);
+          dbg.restore();
         }
 
-        //draw on 2D canvas for hands
-        dbg.clearRect(0, 0, dbg.canvas.width, dbg.canvas.height);
-        dbg.save();
-        dbg.fillStyle = "red";
-        dbg.strokeStyle = "red";
-        //   dbg.drawImage(capture,0,0);
-        drawHands(myHands);
-        dbg.restore();
-        dbg.save();
-        dbg.fillStyle = "red";
-        //   dbg.fillText(statusText,2,60);
-        dbg.restore();
 
         this.renderer.render(this.scene, this.camera);
     }
@@ -311,14 +315,23 @@ class Game {
                 }
                 // create cube
             } else {
+                // let position = new THREE.Vector3();
+                // position.copy(intersect.point).add(intersect.face.normal);
+                // position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+                // let uuid = generateUUID();
+                // let color = game.player.color;
+
+                // game.update("add", { position, color, uuid }); // update local
+                // game.player.socket.emit("add", { position, color, uuid }); // update global
+
 
                 let position = new THREE.Vector3()
                 position.copy(intersect.point).add(intersect.face.normal);
                 position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
                 let uuid = generateUUID()
                 let color = game.player.color
-                let params = { position, color, uuid }
 
+                let params = { position, color, uuid }
 
                 game.state['blocks'][uuid] = params // need to update local state 
                 game.update("add", params) // update local 
@@ -349,206 +362,106 @@ class Game {
         }
     }
 
-    handMovementCallback(position) {
-        //do things when hand moves
+onPointerMove(event) {
+    // console.log('Running onPointerMove.')
+    //moved this functionality to hand
+    game.pointer.set(
+      (event.clientX / window.innerWidth) * 2 - 1,
+      -(event.clientY / window.innerHeight) * 2 + 1
+    );
+    game.raycaster.setFromCamera(game.pointer, game.camera);
+    const intersects = game.raycaster.intersectObjects(game.objects, false);
+    if (intersects.length > 0) {
+      const intersect = intersects[0];
+      game.player.rollOverMesh.position
+        .copy(intersect.point)
+        .add(intersect.face.normal);
+      game.player.rollOverMesh.position
+        .divideScalar(50)
+        .floor()
+        .multiplyScalar(50)
+        .addScalar(25);
+      // console.log('Update socket:: ', game.player.updateSocket)
+      game.player.position = game.player.rollOverMesh.position;
+      game.player.updateSocket();
+      // game.player.socket.emit("update", { uuid: game.player.id, position: game.player.rollOverMesh.position, color: game.player.color });
+    }
+}
 
+onPointerDown(event) {
+    //this functionality is tied to hand now
+    game.pointer.set(
+      (event.clientX / window.innerWidth) * 2 - 1,
+      -(event.clientY / window.innerHeight) * 2 + 1
+    );
+    game.raycaster.setFromCamera(game.pointer, game.camera);
+    const intersects = game.raycaster.intersectObjects(game.objects, false);
+
+    if (intersects.length > 0) {
+      const intersect = intersects[0];
+      // delete cube
+      if (game.isShiftDown) {
+        if (intersect.object !== game.plane) {
+          game.scene.remove(intersect.object);
+          game.objects.splice(game.objects.indexOf(intersect.object), 1);
+          game.player.socket.emit("remove", { uuid: intersect.object.uuid });
+        }
+        // create cube
+      } else {
+        let position = new THREE.Vector3();
+        position.copy(intersect.point).add(intersect.face.normal);
         position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
-        // console.log('position:: ', position)
-        this.player.rollOverMesh.position.copy(position);
+        let uuid = generateUUID();
+        let color = game.player.color;
 
-        /* for (const object of game.objects) {
-          if (position.equals(object.position)) {
-            object.material =
-              object.material.clone(); 
-            object.material.color.setHex(0xffffff);
-            console.log("Found intersection!");
-          }
-        } */
-
-        this.raycaster.set(position, this.down);
-        const intersects = this.raycaster.intersectObjects(this.objects, false);
-
-        if (intersects.length > 0) {
-            const intersect = intersects[0];
-            // delete cube
-            if (this.isShiftDown) {
-                if (intersect.object !== this.plane) {
-                    this.scene.remove(intersect.object);
-                    this.objects.splice(this.objects.indexOf(intersect.object), 1);
-                    this.player.socket.emit("remove", { uuid: intersect.object.uuid });
-                }
-                // create cube
-            } else {
-                let position = new THREE.Vector3();
-                position.copy(intersect.point).add(intersect.face.normal);
-                position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
-                let uuid = generateUUID();
-                let color = this.player.color;
-
-                let params = { position, color, uuid }
-
-                this.update("add", { position, color, uuid }); // update local
-                this.player.socket.emit("add", { position, color, uuid }); // update global
-
-
-                game.state['blocks'][uuid] = params // need to update local state 
-                wfcDone = false
-                wfcModifiers.push(params) // event loop wfc 
-
-            }
-        }
-
-
-
-        requestAnimationFrame(function() {
-            game.animate();
-        });
-        this.updateRemotePlayers(/*dt*/);
-
-        if (handposeModel && videoDataLoaded) {
-            // model and video both loaded
-            handposeModel.estimateHands(capture).then(function(_hands) {
-                // we're handling an async promise
-                // best to avoid drawing something here! it might produce weird results due to racing
-                myHands = _hands; // update the global myHands object with the detected hands
-                if (!myHands.length) {
-                    // haven't found any hands
-                    statusText = "Show some hands!";
-                } else {
-                    // display the confidence, to 3 decimal places
-                    statusText =
-                        "Confidence: " +
-                        Math.round(myHands[0].handInViewConfidence * 1000) / 1000;
-                }
-
-                // tell the server about our updates!
-                game.player.hands = myHands;
-                // console.log('Player hand:: ', game.player.hands)
-                if (game.player.hands[0])
-                    game.player.hands[0].landmarks.forEach((l) => (l[2] += 200));
-                game.player.updateSocket();
-            });
-        }
-
-        //draw on 2D canvas for hands
-        dbg.clearRect(0, 0, dbg.canvas.width, dbg.canvas.height);
-        dbg.save();
-        dbg.fillStyle = "red";
-        dbg.strokeStyle = "red";
-        //   dbg.drawImage(capture,0,0);
-        drawHands(myHands);
-        dbg.restore();
-        dbg.save();
-        dbg.fillStyle = "red";
-        //   dbg.fillText(statusText,2,60);
-        dbg.restore();
-
-        this.renderer.render(this.scene, this.camera);
+        game.update("add", { position, color, uuid }); // update local
+        game.player.socket.emit("add", { position, color, uuid }); // update global
+      }
     }
+}
 
-    onPointerMove(event) {
-        // console.log('Running onPointerMove.')
-        //moved this functionality to hand
-        /* game.pointer.set(
-          (event.clientX / window.innerWidth) * 2 - 1,
-          -(event.clientY / window.innerHeight) * 2 + 1
-        );
-        game.raycaster.setFromCamera(game.pointer, game.camera);
-        const intersects = game.raycaster.intersectObjects(game.objects, false);
-        if (intersects.length > 0) {
-          const intersect = intersects[0];
-          game.player.rollOverMesh.position
-            .copy(intersect.point)
-            .add(intersect.face.normal);
-          game.player.rollOverMesh.position
-            .divideScalar(50)
-            .floor()
-            .multiplyScalar(50)
-            .addScalar(25);
-          // console.log('Update socket:: ', game.player.updateSocket)
-          game.player.position = game.player.rollOverMesh.position;
-          game.player.updateSocket();
-          // game.player.socket.emit("update", { uuid: game.player.id, position: game.player.rollOverMesh.position, color: game.player.color });
-        } */
-    }
 
-    onPointerDown(event) {
-        //this functionality is tied to hand now
-        /* game.pointer.set(
-          (event.clientX / window.innerWidth) * 2 - 1,
-          -(event.clientY / window.innerHeight) * 2 + 1
-        );
-        game.raycaster.setFromCamera(game.pointer, game.camera);
-        const intersects = game.raycaster.intersectObjects(game.objects, false);
 
-        if (intersects.length > 0) {
-          const intersect = intersects[0];
-          // delete cube
-          if (game.isShiftDown) {
-            if (intersect.object !== game.plane) {
-              game.scene.remove(intersect.object);
-              game.objects.splice(game.objects.indexOf(intersect.object), 1);
-              game.player.socket.emit("remove", { uuid: intersect.object.uuid });
+handMovementCallback(position) {
+    console.log('Executing handMovementCallback!')
+    //do things when hand moves
+
+    position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+    // console.log('position:: ', position)
+    this.player.rollOverMesh.position.copy(position);
+
+    /* for (const object of game.objects) {
+      if (position.equals(object.position)) {
+        object.material =
+          object.material.clone(); 
+        object.material.color.setHex(0xffffff);
+        console.log("Found intersection!");
+      }
+    } */
+
+    this.raycaster.set(position, this.down);
+    const intersects = this.raycaster.intersectObjects(this.objects, false);
+
+    if (intersects.length > 0) {
+        const intersect = intersects[0];
+        // delete cube
+        if (this.isShiftDown) {
+            if (intersect.object !== this.plane) {
+                this.scene.remove(intersect.object);
+                this.objects.splice(this.objects.indexOf(intersect.object), 1);
+                this.player.socket.emit("remove", { uuid: intersect.object.uuid });
             }
             // create cube
-          } else {
+        } else {
             let position = new THREE.Vector3();
             position.copy(intersect.point).add(intersect.face.normal);
             position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
             let uuid = generateUUID();
-            let color = game.player.color;
+            let color = this.player.color;
 
-            game.update("add", { position, color, uuid }); // update local
-            game.player.socket.emit("add", { position, color, uuid }); // update global
-          }
-        } */
-    }
-
-    onDocumentKeyDown(event) {
-        switch (event.keyCode) {
-            case 16:
-                game.isShiftDown = true;
-                break;
+            this.update("add", { position, color, uuid }); // update local
+            this.player.socket.emit("add", { position, color, uuid }); // update global
         }
     }
-
-    onDocumentKeyUp(event) {
-        switch (event.keyCode) {
-            case 16:
-                game.isShiftDown = false;
-                break;
-        }
-    }
-
-/*    handMovementCallback(position) {
-        //do things when hand moves
-
-        position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
-        // console.log('position:: ', position)
-        this.player.rollOverMesh.position.copy(position);
-        this.raycaster.set(position, this.down);
-        const intersects = this.raycaster.intersectObjects(this.objects, false);
-
-        if (intersects.length > 0) {
-            const intersect = intersects[0];
-            // delete cube
-            if (this.isShiftDown) {
-                if (intersect.object !== this.plane) {
-                    this.scene.remove(intersect.object);
-                    this.objects.splice(this.objects.indexOf(intersect.object), 1);
-                    this.player.socket.emit("remove", { uuid: intersect.object.uuid });
-                }
-                // create cube
-            } else {
-                let position = new THREE.Vector3();
-                position.copy(intersect.point).add(intersect.face.normal);
-                position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
-                let uuid = generateUUID();
-                let color = this.player.color;
-
-                this.update("add", { position, color, uuid }); // update local
-                this.player.socket.emit("add", { position, color, uuid }); // update global
-            }
-        }
-    }*/
+}
 }
