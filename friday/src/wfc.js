fc0548f1056f76fc888b74f3d6ccd68fe3f82b2e
Sayjel Vijay Patel <sayjel.v.patel@gmail.com>
2022-07-08 11:44:14 +0800

    wfc backend

--- friday/src/wfc.js
+++ friday/src/wfc.js
@@ -52,9 +52,32 @@ function initialize(state) {
 
 
     let grid = startOver(state);
+    return { rules, grid }
 
 
-    return {rules, grid}
+
+}
+
+
+
+
+
+function run(grid, rules, steps) {
+
+
+  let {DIM} = state 
+
+  for (var i = steps; i >= 0; i--){
+
+    grid = step(grid,rules, DIM)
+
+
+
+  }
+
+
+
+
 
 
 
@@ -64,25 +87,46 @@ function initialize(state) {
 
 
 
-function run() {
+function step(grid, rules, DIM) {
 
 
 
-}
+  /*
+    // let DIM = 20
 
+    // step 1: check end condition
+    if (wfcDone) return;
 
+    // step 2: check modifiers
+    wfcModifiers.forEach((o) => wfcModify(game.state, grid, o));
+    wfcModifiers = [];
 
-function step() {
+    // step 3: run wfc
 
+    */
 
+    let gridCopy = grid.slice(); // Make a copy of grid
+    gridCopy = gridCopy.filter((a) => !a.collapsed); // Remove any collapsed cells
 
+    if (gridCopy.length == 0) {
+        // The algorithm has completed if everything is collapsed
+        wfcDone = true;
+        return;
+    }
+
+    grid = wfc({ grid, gridCopy, p, DIM });
+
+    // step 4: draw tiles
+    drawTiles(p, DIM);?
+
+    return grid 
 
 }
 
 
 function startOver(state) {
-    
-    let { blockSize, DIM, blocks } = state 
+
+    let { blockSize, DIM, blocks } = state
 
     let grid = [];
 
@@ -92,75 +136,203 @@ function startOver(state) {
 
     placeBlockCells(game.state, Object.values(blocks));
 
-    return grid 
+    return grid
+}
+
+
+class Cell {
+  constructor(value) {
+    // Is it collapsed?
+    this.collapsed = false;
+
+    // Initial options via constructor
+    if (value instanceof Array) {
+      this.options = value;
+    } else {
+      // or all options to start
+      this.options = [];
+      for (let i = 0; i < value; i++) {
+        this.options[i] = i;
+      }
+    }
+  }
 }
 
 
 
 // Tile class
 class Tile {
-  constructor(edges) {
-    // Image
-    // this.img = img;
-    // Edges
-    this.edges = edges;
-    // Valid neighbors
-    this.up = [];
-    this.right = [];
-    this.down = [];
-    this.left = [];
-  }
-  
-  // Find the valid neighbors
-  analyze(tiles) {
-    for (let i = 0; i < tiles.length; i++) {
-      let tile = tiles[i];
-      // UP
-      if (compareEdge(tile.edges[2], this.edges[0])) {
-        this.up.push(i);
-      }
-      // RIGHT
-      if (compareEdge(tile.edges[3], this.edges[1])) {
-        this.right.push(i);
-      }
-      // DOWN
-      if (compareEdge(tile.edges[0], this.edges[2])) {
-        this.down.push(i);
-      }
-      // LEFT
-      if (compareEdge(tile.edges[1], this.edges[3])) {
-        this.left.push(i);
-      }
+    constructor(edges) {
+        // Image
+        // this.img = img;
+        // Edges
+        this.edges = edges;
+        // Valid neighbors
+        this.up = [];
+        this.right = [];
+        this.down = [];
+        this.left = [];
     }
-  }
-  
-  // Rotate a tile and its edges to create a new one
-  rotate(num, p5) {
-  
-    // Rotate edges
-    const newEdges = [];
-    const len = this.edges.length;
-    for (let i = 0; i < len; i++) {
-      newEdges[i] = this.edges[(i - num + len) % len];
+
+    // Find the valid neighbors
+    analyze(tiles) {
+        for (let i = 0; i < tiles.length; i++) {
+            let tile = tiles[i];
+            // UP
+            if (compareEdge(tile.edges[2], this.edges[0])) {
+                this.up.push(i);
+            }
+            // RIGHT
+            if (compareEdge(tile.edges[3], this.edges[1])) {
+                this.right.push(i);
+            }
+            // DOWN
+            if (compareEdge(tile.edges[0], this.edges[2])) {
+                this.down.push(i);
+            }
+            // LEFT
+            if (compareEdge(tile.edges[1], this.edges[3])) {
+                this.left.push(i);
+            }
+        }
     }
-    return new Tile(newEdges);
-  }
+
+    // Rotate a tile and its edges to create a new one
+    rotate(num, p5) {
+
+        // Rotate edges
+        const newEdges = [];
+        const len = this.edges.length;
+        for (let i = 0; i < len; i++) {
+            newEdges[i] = this.edges[(i - num + len) % len];
+        }
+        return new Tile(newEdges);
+    }
 }
 
 // Function to reverse a string
 function reverseString(s) {
-  let arr = s.split("");
-  arr = arr.reverse();
-  return arr.join("");
+    let arr = s.split("");
+    arr = arr.reverse();
+    return arr.join("");
 }
 
 // Function to compare two edges
 function compareEdge(a, b) {
-  return a == reverseString(b);
+    return a == reverseString(b);
+}
+
+
+function randomElement(arr){
+
+
+  return arr[Math.floor(Math.random()*arr.length)]
+
+
 }
 
 
 
+function wfc({ grid, gridCopy, p, DIM }) {
+    // Pick a cell with least entropy
+
+    // Sort by entropy
+    gridCopy.sort((a, b) => {
+        return a.options.length - b.options.length;
+    });
+
+    // Keep only the lowest entropy cells
+
+    let len = gridCopy[0].options.length;
+    let stopIndex = 0;
+    for (let i = 1; i < gridCopy.length; i++) {
+        if (gridCopy[i].options.length > len) {
+            stopIndex = i;
+            break;
+        }
+    }
+    if (stopIndex > 0) gridCopy.splice(stopIndex);
+
+    // Collapse a cell
+    const cell = randomElement(gridCopy)
+    cell.collapsed = true;
+    const pick = randomElement(cell.options)
+    cell.options = [pick];
+
+    if (cell.options[0] === undefined) {
+        return;
+    }
+
+    // Calculate entropy
+    const nextGrid = [];
+    for (let j = 0; j < DIM; j++) {
+        for (let i = 0; i < DIM; i++) {
+            let index = i + j * DIM;
+            if (grid[index].collapsed) {
+                nextGrid[index] = grid[index];
+            } else {
+                let options = new Array(tiles.length).fill(0).map((x, i) => i);
+                // Look up
+                if (j > 0) {
+                    let up = grid[i + (j - 1) * DIM];
+                    let validOptions = [];
+                    for (let option of up.options) {
+                        let valid = tiles[option].down;
+                        validOptions = validOptions.concat(valid);
+                    }
+                    checkValid(options, validOptions);
+                }
+                // Look right
+                if (i < DIM - 1) {
+                    let right = grid[i + 1 + j * DIM];
+                    if (right == undefined) console.log("right undefined");
+                    let validOptions = [];
+                    for (let option of right.options) {
+                        let valid = tiles[option].left;
+                        validOptions = validOptions.concat(valid);
+                    }
+                    checkValid(options, validOptions);
+                }
+                // Look down
+                if (j < DIM - 1) {
+                    let down = grid[i + (j + 1) * DIM];
+                    let validOptions = [];
+                    for (let option of down.options) {
+                        // console.log(tiles[option])
+                        if (tiles[option] == undefined) {
+                            console.log(down);
+                            console.log(option);
+                        }
+                        let valid = tiles[option].up; // getting erros here
+                        validOptions = validOptions.concat(valid);
+                    }
+                    checkValid(options, validOptions);
+                }
+                // Look left
+                if (i > 0) {
+                    let left = grid[i - 1 + j * DIM];
+                    let validOptions = [];
+                    for (let option of left.options) {
+                        let valid = tiles[option].right;
+                        validOptions = validOptions.concat(valid);
+                    }
+                    checkValid(options, validOptions);
+                }
+
+                // I could immediately collapse if only one option left?
+                nextGrid[index] = new Cell(options);
+            }
+        }
+    }
+
+    grid = nextGrid;
+
+    return grid;
+}
+
+
+
+/*
 
 
 function initP5js(scene) {
@@ -512,3 +684,5 @@ function wfc({ grid, gridCopy, p, DIM }) {
 
     return grid;
 }
+
+*/
